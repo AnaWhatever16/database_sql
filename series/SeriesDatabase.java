3b4475e56e7f49ee271b598dc763b95801b93ad5
AnaWhatever16 <anacasadofauli@gmail.com>
2021-05-23 10:53:22 +0200

    Open and close connection with SQL (remember to set the sql script with server parameters)

--- series/SeriesDatabase.java
+++ series/SeriesDatabase.java
@@ -1,21 +1,66 @@
 package series;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class SeriesDatabase {
-
+	private boolean dbOpen_;
+	private Connection conn;
+	
 	public SeriesDatabase() {
-
+		dbOpen_ = false;
 	}
 
 	public boolean openConnection() {
-		
-		return false;
+		if(!dbOpen_) {
+			try {
+				String drv = "com.mysql.cj.jdbc.Driver";
+				Class.forName(drv);
+				System.out.println("Driver cargado correctamente");
+			} catch (ClassNotFoundException e){
+				System.out.println("Error, el driver no se ha encontrado");
+				e.printStackTrace();
+				return false;
+			}
+			
+			try {
+				String serverAddress = "localhost:3306";
+				String db = "series";
+				String user = "series_user";
+		        String pass = "series_pass";
+				String url = "jdbc:mysql://" + serverAddress + "/" + db;
+				conn = DriverManager.getConnection(url, user, pass);					
+				dbOpen_ = true;
+				System.out.println("Base de Datos cargada correctamente");
+				return true;
+			}catch (SQLException e) {
+				System.out.println("Error, la base de datos no se ha encontrado");
+				e.printStackTrace();
+				return false;
+			}
+		}else {
+			return false;
+		}
 	}
 
 	public boolean closeConnection() {
-		return false;
+		if(dbOpen_) {
+			try {
+				conn.close();	
+				System.out.println("Base de Datos cerrada correctamente");
+				return true;
+			} catch(SQLException e) {
+				System.out.println("Error, la base de datos no se ha encontrado");
+				return false;
+			}			
+		}else {
+			return true;
+		}
 	}
 
 	public boolean createTableCapitulo() {
+		openConnection();
 		System.out.println("Hello World");
 		return false;
 	}
