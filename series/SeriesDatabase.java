72d4ccd4e51238839a26d6f62dc2f52031be64dc
AnaWhatever16 <anacasadofauli@gmail.com>
2021-05-23 20:22:16 +0200

    some changes on the connection handling

--- series/SeriesDatabase.java
+++ series/SeriesDatabase.java
@@ -5,22 +5,21 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class SeriesDatabase {
-	private boolean dbOpen_;
-	private Connection conn;
+	private static Connection conn_ = null;
 	
 	public SeriesDatabase() {
-		dbOpen_ = false;
+		
 	}
 
 	public boolean openConnection() {
-		if(!dbOpen_) {
+		if(conn_ == null) {
 			try {
 				String drv = "com.mysql.cj.jdbc.Driver";
 				Class.forName(drv);
 				System.out.println("Driver cargado correctamente");
-			} catch (ClassNotFoundException e){
+			} catch (ClassNotFoundException _e){
 				System.out.println("Error, el driver no se ha encontrado");
-				e.printStackTrace();
+				_e.printStackTrace();
 				return false;
 			}
 			
@@ -30,28 +29,30 @@ public class SeriesDatabase {
 				String user = "series_user";
 		        String pass = "series_pass";
 				String url = "jdbc:mysql://" + serverAddress + "/" + db;
-				conn = DriverManager.getConnection(url, user, pass);					
-				dbOpen_ = true;
+				conn_ = DriverManager.getConnection(url, user, pass);					
 				System.out.println("Base de Datos cargada correctamente");
 				return true;
-			}catch (SQLException e) {
+			}catch (SQLException _e) {
+				conn_ = null;
 				System.out.println("Error, la base de datos no se ha encontrado");
-				e.printStackTrace();
+				_e.printStackTrace();
 				return false;
 			}
 		}else {
+			System.out.println("Base de Datos cargada anteriormente");
 			return false;
 		}
 	}
 
 	public boolean closeConnection() {
-		if(dbOpen_) {
+		if(conn_ != null) {
 			try {
-				conn.close();	
+				conn_.close();	
 				System.out.println("Base de Datos cerrada correctamente");
 				return true;
-			} catch(SQLException e) {
+			} catch(SQLException _e) {
 				System.out.println("Error, la base de datos no se ha encontrado");
+				_e.printStackTrace();
 				return false;
 			}			
 		}else {
@@ -66,34 +67,42 @@ public class SeriesDatabase {
 	}
 
 	public boolean createTableValora() {
+		openConnection();
 		return false;
 	}
 
 	public int loadCapitulos(String fileName) {
+		openConnection();
 		return 0;
 	}
 
 	public int loadValoraciones(String fileName) {
+		openConnection();
 		return 0;
 	}
 
 	public String catalogo() {
+		openConnection();
 		return null;
 	}
 	
 	public String noHanComentado() {
+		openConnection();
 		return null;
 	}
 
 	public double mediaGenero(String genero) {
+		openConnection();
 		return 0.0;
 	}
 	
 	public double duracionMedia(String idioma) {
+		openConnection();
 		return 0.0;
 	}
 
 	public boolean setFoto(String filename) {
+		openConnection();
 		return false;
 	}
 
