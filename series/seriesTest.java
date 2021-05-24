b7b03109e9c37f1b0084aa52e03597b7f30c6068
AnaWhatever16 <anacasadofauli@gmail.com>
2021-05-24 20:25:52 +0200

    Unit test created

---
+++ series/seriesTest.java
@@ -0,0 +1,14 @@
+package series;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class seriesTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
